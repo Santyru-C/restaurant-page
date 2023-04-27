589ff3c4e4717c7896dd57d55525c74f7bb06275
Santyru <santiago0988@hotmail.com>
2023-04-27 15:30:24 -0300

    import menu imgs

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -1,4 +1,7 @@
 import * as devTools from "../generators";
+import Meal1 from "./meal_imgs/meal1.png";
+import Meal2 from "./meal_imgs/meal2.png";
+import Meal3 from "./meal_imgs/meal3.png";
 
 function generateMealMenu(image, name, description, price) {
 
