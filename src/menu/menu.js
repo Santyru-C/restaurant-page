b3658212c9cbc5bfd69ba4621fffa007dee2cc04
Santyru <santiago0988@hotmail.com>
2023-04-27 16:07:10 -0300

    add menu generator function

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -1,8 +1,19 @@
 import * as devTools from "../generators";
+import { generateBundleDiv } from "../generators";
 import Meal1 from "./meal_imgs/meal1.png";
 import Meal2 from "./meal_imgs/meal2.png";
 import Meal3 from "./meal_imgs/meal3.png";
 
-function generateMealMenu(image, name, description, price) {
+function generateMealMenu(imageSrc, name, description, price) {
+  const mealImage = devTools.generateImage(imageSrc, "meal-image");
+  const mealName = devTools.generateElementWithText("div", "meal-name", name);
+  const mealDesc = devTools.generateElementWithText("div", "meal-desc", description);
+  const mealInfo = devTools.generateBundleDiv([mealName, mealDesc], "meal-info");
+  const mealPrice = devTools.generateElementWithText("div", "meal-price", price);
 
+  return generateBundleDiv([mealImage, mealInfo, mealPrice], "meal-menu");
 }
+
+const menu1 = generateMealMenu(Meal1, "asdfsdaf", "sadfasdf", "200");
+
+export default menu1;
