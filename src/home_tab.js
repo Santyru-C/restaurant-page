5e9ed31733f6b76b7724b3e460cb25eb1b2a7178
Santyru <santiago0988@hotmail.com>
2023-04-27 15:31:27 -0300

    solve minor bug

--- src/home_tab.js
+++ src/home_tab.js
@@ -1,42 +1,15 @@
 // generate a three part grid that has text on the sides
 // and contains a png on the center cell
-import MainImage from "./main_dish.jpg";
+import * as devTools from "./generators";
+import MainImage from "./main_dish.png";
 
-function generateElement(element, template) {
-  const newElement = document.createElement(element);
-  newElement.classList.add(template[0]);
-  newElement.textContent = template[1];
-  return newElement;
-}
+const text1 = devTools.generateElement("div", "home-text1");
+const text2 = devTools.generateElement("div", "home-text2");
+const text3 = devTools.generateElement("div", "flavour-text");
+text1.textContent = "Always Delivering";
+text2.textContent = "Exceptionally  Delicious Meals";
+text3.textContent = "Lorem ipsum dolor sit amet.";
+const textBundle = devTools.generateBundleDiv([text1, text2, text3], "home-text-container");
+const image = devTools.generateImage(MainImage, "home-image");
 
-function generateImage(src) {
-  const imageContainer = document.createElement("div");
-  imageContainer.classList.add("home-image-container");
-
-  const homeMealImage = document.createElement("img");
-  homeMealImage.classList.add("home-image");
-  homeMealImage.src = src;
-
-  imageContainer.appendChild(homeMealImage);
-  return imageContainer;
-}
-
-function generateBundleDiv(childElements, className) {
-  const bundleDiv = document.createElement("div");
-  bundleDiv.classList.add(className);
-
-  childElements.forEach((element) => {
-    bundleDiv.appendChild(element);
-  });
-
-  return bundleDiv;
-}
-
-const textTemplate1 = ["restaurant-name-text", "Enjoy Our Delicious Meals"];
-const textTemplate2 = ["flavour-text", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."];
-const text1 = generateElement("div", textTemplate1);
-const text2 = generateElement("div", textTemplate2);
-const textBundle = generateBundleDiv([text1, text2], "home-text-container");
-const image = generateImage(MainImage);
-
-export default generateBundleDiv([image, textBundle], "home-container");
+export default devTools.generateBundleDiv([textBundle, image], "home-container");
