2e4417882aafe2f1a1198c649f24e9e1e9d1909a
Santyru <santiago0988@hotmail.com>
2023-04-29 17:24:35 -0300

    add event listeners for all tabas

--- src/index.js
+++ src/index.js
@@ -16,17 +16,51 @@ const layoutGrid = devTools.generateElement("div", "layout-grid");
 const logo = devTools.generateElement("h1", "logo");
 logo.textContent = "IL RISTORANTE";
 
-const tabLabels = ["Home", "Menu", "Contact"];
-const tabContainer = devTools.generateElement("div", "tab-container");
+const tabLabels = ["home", "menu", "contact"];
+const tabContainer = devTools.generateElement("ul", "tab-container");
 tabLabels.forEach((label) => {
-  const tab = devTools.generateElement("button", "tab");
-  tab.textContent = label;
+  const tabClass = `tab-${label}`;
+  const tabText = label.charAt(0).toUpperCase() + label.slice(1);
+  const tab = devTools.generateElementWithText("li", tabClass, tabText);
   tabContainer.appendChild(tab);
 });
 
 const headerContainer = devTools.generateElement("div", "header-container");
+const contentContainer = devTools.generateElement("div", "content-container");
 headerContainer.appendChild(logo);
 headerContainer.appendChild(tabContainer);
 mainContent.appendChild(layoutGrid);
 layoutGrid.appendChild(headerContainer);
-layoutGrid.appendChild(contact);
+layoutGrid.appendChild(contentContainer);
+
+let currentTab = "home";
+
+function switchTab(newTab) {
+  if (newTab !== currentTab) {
+    contentContainer.removeChild(contentContainer.lastChild);
+    switch (newTab) {
+      case "home":
+        console.log(home);
+        contentContainer.appendChild(home);
+        currentTab = "home";
+        break;
+      case "menu":
+        contentContainer.appendChild(menu);
+        currentTab = "menu";
+        break;
+      case "contact":
+        contentContainer.appendChild(contact);
+        currentTab = "contact";
+        break;
+      default:
+        console.log("nain");
+    }
+  }
+}
+const homeTab = document.getElementsByClassName("tab-home")[0];
+const menuTab = document.getElementsByClassName("tab-menu")[0];
+const contactTab = document.getElementsByClassName("tab-contact")[0];
+homeTab.addEventListener("click", () => { switchTab("home"); });
+menuTab.addEventListener("click", () => { switchTab("menu"); });
+contactTab.addEventListener("click", () => { switchTab("contact"); });
+contentContainer.appendChild(home);
